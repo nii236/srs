3c97606687d0f051955ef4908b9076fbd3fefdb5
John Nguyen <jnguyen@thefrontiergroup.com.au>
2017-02-18 19:20:59 +0800

    adds gpg lib

--- cmd/auth.go
+++ cmd/auth.go
@@ -9,9 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// registerCmd represents the register command
-var registerCmd = &cobra.Command{
-	Use:   "register",
+// authCmd represents the auth command
+var authCmd = &cobra.Command{
+	Use:   "auth",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,21 +21,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
-		fmt.Println("register called")
+		fmt.Println("auth called")
 	},
 }
 
 func init() {
-	userCmd.AddCommand(registerCmd)
+	userCmd.AddCommand(authCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// authCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// authCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
